8ddb1410864b0726c52e6df7417030090a26ca3b
Carlos Eduardo Cervantes Arteaga <89818143+carlos-cerv@users.noreply.github.com>
2023-05-29 14:18:48 -0600

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+dogs=['Sparky', 'Hunter', 'Loki', 'Astro', 'Sparky', 'Rocky', 'Rocky', 'Toby', 'Chelsea',
+      'Maple', 'Maya', 'Loki', 'Sparky', 'Toby', 'Sparky', 'Dexter', 'Fido', 'Sparky']
+
+dog_dict=defaultdict(int)
+for dog in dogs:
+  dog_dict[dog] += 1
+
+print('Rocky->', dog_dict['Rocky'])
+print('Sparky->', dog_dict['Sparky'])
+print(list(dog_dict.keys()))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
